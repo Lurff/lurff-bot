6ec357a73e183ff3f1a3d4aa1b96f75b6fcf62d3
Lurff <98714395+Lurff@users.noreply.github.com>
2022-09-02 19:18:59 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const client = new Client({
     intents:["GUILDS",
     "GUILD_MEMBERS",
     "GUILD_MESSAGES",
-    "GUILD_VOICE_STATES"]
+    "GUILD_VOICE_STATES",
+    "GUILD_BANS"]
 }) 
 
 client.embed = require("./utils/embed.js")
@@ -19,23 +20,21 @@ readdirSync("./events").forEach(async file => {
 //Command Loader
 client.commands = new Collection()
 readdirSync("./komutlar").forEach(category => {
-
-    readdirSync(`./komutlar/${category}`).forEach(file => {
-      const command = require(`./komutlar/${category}/${file}`)
-        client.commands.set(command.data.name,command)
-    })
+    
+    const command = require(`./komutlar/${category}`)
+    client.commands.set(command.data.name,command)
 })
 
 // Utils İmported
 
 const guildMemberAdd = require("./utils/auto_role.js")
 const music = require("./utils/voice_room_config.js")
-
+const ticket = require("./utils/ticket.js")
 
 client.on("ready",() => {
     guildMemberAdd(client)
     music(client)
+    ticket(client)
 })
-
 
 client.login(process.env.token)
